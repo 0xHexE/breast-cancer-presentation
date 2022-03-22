9721f3f3204c6df8605f2e483dd9bc106b1bf1a6
Omkar Yadav <omkar@anthaathi.org>
2022-03-22 23:26:54 +0400

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+// @ts-ignore
+import Reveal from 'reveal.js';
+// @ts-ignore
+import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+import 'reveal.js/dist/reveal.css';
+import 'reveal.js/dist/theme/sky.css';
+
+let deck = new Reveal({
+    plugins: [ Markdown ]
+});
+
+deck.initialize();
+
+// —Introduction (Breast cancer is most common cancer affecting females)
+// —Steps of downloading the app.
+// —Risk factor screenshots
+// —symptoms screenshots (each symptom on separate slide)
+// —steps of self breast examination (each step on different slide)
+// —concluding slide : your health is true beauty, let’s care for it.
